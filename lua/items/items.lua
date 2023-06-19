40aa63b0688f6cab4af12ad79d117a05f2468a77
geliusheng <408963516@qq.com>
2023-06-19 16:46:12 +0800

    xiao 优化

---
+++ lua/items/items.lua
@@ -0,0 +1,7 @@
+local Items={
+    {Id="1001",Name="tid_item_1001",EffectType=1,Effect={100,1,10,},IsUse=true,},
+    {Id="1002",Name="tid_item_1002",EffectType=2,Effect={200,1,20,},IsUse=false,},
+    {Id="aa",Name="",EffectType=0,Effect={},IsUse=false,},
+    {Id="bb",Name="",EffectType=0,Effect={},IsUse=false,},
+}
+return Items
